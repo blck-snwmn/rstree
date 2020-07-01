8626f2e8602e72e5f2578cece9ba6ac84bbf87a0
blck-snwmn <whitesnowmancreator@gmail.com>
2020-07-02 01:05:31 +0900

    Show paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
-pub fn hello() {
-    println!("Hello, world!");
+use std::fs;
+use std::path::Path;
+
+pub fn get_tree(path: &Path) -> std::io::Result<()> {
+    if !path.is_dir() {
+        return Ok(());
+    }
+    let paths = fs::read_dir(path)?;
+    for path in paths {
+        path.map(|e| println!("{:?}", e.path()))?;
+    }
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
-fn main() {
-    rstree::hello()
+fn main() -> std::io::Result<()> {
+    let path = std::env::current_dir()?;
+    rstree::get_tree(&path)
 }
